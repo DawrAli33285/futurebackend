7e8c24a73cc3ca008a6354ec4935781755daf850
DawrAli33285 <lemightyeagle@gmail.com>
2025-11-12 15:33:15 +0500

    new

--- connection/connection.js
+++ connection/connection.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
 
-const connect=mongoose.connect('mongodb://127.0.0.1/astrology')
+const connect=mongoose.connect('mongodb://localhost:27017/astrology')
 
 module.exports=connect;
